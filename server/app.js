6e7b100b703ceeb19f6b9f15bab65a565838f213
clarejacky <clare.jacky@gmail.com>
2015-05-05 12:01:51 -0500

    database set up

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 var express = require('express');
-var app = express();
-
+var bodyParser = require('body-parser');
 var index = require('./routes/index');
-app.use("/", index);
+var app = express();
 
 
 var server = app.listen(3000, function () {
     var port = server.address().port;
     console.log("Listening on port: ", port);
 });
+
+app.use(bodyParser.json());
+app.use('/', index);
+module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -8,11 +8,28 @@ mongoose.connect('mongodb://localhost/basic_walking_skeleton');
 var Cat = mongoose.model('Cat', {name:String});
 
 
-router.get('/', function(req, res, next){
-    console.log("Here is a console log");
-    var file  = req.params[0] || 'views.index.html';
-    res.sendFile(path.join(_))
-    next();
+router.post('/add', function(request, response, next){
+    var kitty = new Cat({name: request.body.name});
+    kitty.save(function(err){
+        if(err)console.log('meow %s', err);
+        response.send(kitty.toJSON());
+        next();
+    });
+});
+
+router.get('/cats', function(request, response, next){
+    return Cat.find({}).exec(function(err, cats){
+        if(err) throw new Error(err);
+        response.send(JSON.stringify(cats));
+        next();
+    });
+
+});
+
+router.get("/*", function(req, res, next){
+    var file = req.params[0] || 'views/index.html';
+    res.sendFile(path.join(__dirname, '../public', file));
+    //next();
 });
 
 module.exports = router;
